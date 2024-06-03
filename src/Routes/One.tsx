61c04f7cd88dbe833152b56551f87102c4e4a0e6
Esmatt123 <105745382+Esmatt123@users.noreply.github.com>
2024-06-03 11:41:15 +0200

    first commit

--- src/Routes/One.tsx
+++ src/Routes/One.tsx
@@ -2,14 +2,13 @@
 import React from 'react';
 
 const One: React.FC = () => {
-  const str = `Ive been self studying programming for over a year now. 
-  Learning about Html, Css, JavaScript, React, C# and the .NET Framework,
-  the reason why I want to take this course is for a couple of reasons,  
-  Ive yet to learn to program in a team and learn about agile/scrum development 
-  This school seems to be very kind to people with special needs, as I myself am 
-  diagnosed with Asperger syndrome and would love the guidance and supportive environment this school would offer. 
-  There may be gaps in my knowledge, although Im confident in my own abilities, I want to see and hear others point of view, and push myself further 
-  If this school offers any kind of recruitment assistance and potential to meet potential future employers, I would love that opportunity to get my foot in the door in the IT industry`
+  const str = `Ive been self studying programming for over two years now. 
+  I took the front end course, and was  very happy with the people I met, the teachers including Michelle, and the projects I got to do, 
+  I am pretty familiar with backend development with Asp.net/C#, but not very familiar with Node.js, and thats why id like to take this course, 
+  to add another framework to my skillset. There may be gaps in my knowledge, although Im confident in my own abilities, 
+  I want to see and hear others point of view, and push myself further 
+  If this school offers any kind of recruitment assistance and potential to meet 
+  potential future employers, I would love that opportunity to get my foot in the door in the IT industry`
 
   return (
     <div>
